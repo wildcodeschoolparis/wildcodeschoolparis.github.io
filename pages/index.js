9cc2a616dfc85e0d30afb587065304310acb03e5
Clement Denis <le.mikmac@gmail.com>
2018-02-26 09:10:24 +0100

    publish build

--- pages/index.js
+++
@@ -1,4 +0,0 @@
-import main from '../component/main'
-import loginForm from '../component/login-form'
-
-export default () => main([ loginForm ])
--- pages/trainer.js
+++
@@ -1,4 +0,0 @@
-import main from '../component/main'
-import onboardingStatus from '../component/onboarding-status'
-
-export default () => main([ onboardingStatus ])
